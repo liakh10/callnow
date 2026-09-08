f60b884beb39ff200995cd41e009aa754603b6e8
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-08 21:05:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0xf0EC2AF4Ed18E3e81d7B25d600cD585e1947f90a",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/lsn_robihood",
   NAME:    "Laptop Shopping Network",
   TICKER:  "$LSN",
